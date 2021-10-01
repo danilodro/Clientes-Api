c694268a5046a7b11e259b443e95d54370cd2dfa
Danilo Oliveira <danilo.oliveira@infracommerce.com.br>
2021-10-01 16:01:02 -0300

    update tz

--- setup/settings.py
+++ setup/settings.py
@@ -100,13 +100,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'America/Sao_Paulo'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
